fd474288a4547c6facb9effc339e43980d2b5105
yurashkurenko <yurashkurenko@users.noreply.github.com>
2025-01-31 17:07:32 +0300

    Add files via upload

--- transformtext.py
+++ transformtext.py
@@ -9,7 +9,7 @@ def process_file(input_file, output_file, transform_function):
     """
 
     try:
-        with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
             for line in infile:
                 transformed_line = transform_function(line)
                 outfile.write(transformed_line)
